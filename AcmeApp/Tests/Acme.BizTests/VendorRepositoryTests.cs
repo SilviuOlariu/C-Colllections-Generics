5c3ee1e82ab88ef5737ca20bf84a802e28c651cf
ENDAVA\solariu <silviu.olariu@endava.com>
2020-05-21 16:44:54 +0300

    inital commit

---
+++ AcmeApp/Tests/Acme.BizTests/VendorRepositoryTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class VendorRepositoryTests
+    {
+        [TestMethod]
+        public void RetrieveDecimalValue()
+        {
+            var repo = new VendorRepository();
+            var expect = 12m;
+
+            var sqlMoke = "connection";
+            var act = repo.RetrieveValue<decimal>(sqlMoke, 12m);
+
+            Assert.AreEqual(expect, act);
+
+
+        }
+
+        [TestMethod]
+        public void RetrieveStringcValue()
+        {
+            var repo = new VendorRepository();
+            var expect = "test";
+
+            var sqlMoke = "connection";
+            var act = repo.RetrieveValue<string>(sqlMoke, "test");
+
+            Assert.AreEqual(expect, act);
+
+
+        }
+    }
+}
